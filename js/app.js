fa4b4a2539f7ff836da708ea4897a082e6dde115
vanmer <60948275+vanmer@users.noreply.github.com>
2020-06-18 16:18:52 +0200

    elements, class names in app.js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
-document.addEventListener('DOMContentLoaded', () => {
-  // select the elements
-  const clear = document.querySelector(".clear");
+// select the elements
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
 
 
+// classes names
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
 
-
-
-
-
-
-
-})
+// display todays date

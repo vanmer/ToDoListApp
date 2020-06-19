2251ee44384cc62c43fd71342d0f768a49b7140f
vanmer <60948275+vanmer@users.noreply.github.com>
2020-06-19 14:19:49 +0200

    LIST array

--- js/app.js
+++ js/app.js
@@ -9,23 +9,33 @@ const CHECK = "fa-check-circle";
 const UNCHECK = "fa-circle-thin";
 const LINE_THROUGH = "lineThrough";
 
+// variables
+let LIST = [];
+let id = 0;
+
 // display todays date
-const options = { weekday : "long", month: "short", day: "numeric" }
+const options = {
+  weekday: "long",
+  month: "short",
+  day: "numeric"
+}
 const today = new Date();
 dateElement.innerHTML = today.toLocaleDateString("en-US", options);
 
 // add to-Do function
 function addToDo(toDo, id, done, trash) {
-  if (trash) { return; }
+  if (trash) {
+    return;
+  }
 
   const DONE = done ? CHECK : UNCHECK;
   const LINE = done ? LINE_THROUGH : "";
 
   const item = `
                 <li class="item">
-                  <i class="fa fa-circle-thin co" job="complete" id="${id}"></i>
-                  <p class="text">${toDo}</p>
-                  <i class="fa fa-trash-o de" job="delete" id="${id}"></i>
+                  <i class="fa ${DONE} co" job="complete" id=${id}></i>
+                  <p class="text ${LINE}">${toDo}</p>
+                  <i class="fa fa-trash-o de" job="delete" id=${id}></i>
                 </li>
                `;
   const position = "beforeend";
@@ -39,7 +49,14 @@ document.addEventListener("keyup", function(event) {
     const toDo = input.value;
     // if the input is not empty
     if (toDo) {
-      addToDo(toDo);
+      addToDo(toDo, id, false, false);
+      LIST.push({
+        name: toDo,
+        id: id,
+        done: false,
+        trash: false
+      });
+      id++;
     }
     input.value = "";
   }
